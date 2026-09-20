2e05ddc82837b4fa8a9c8ac8236cda806be572f6
taigahorima-prog <taiga.horima@nagi-livestock.com>
2026-09-20 20:58:37 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="shinryo-pwa-v5.31";
+const CACHE="shinryo-pwa-v5.32";
 const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
